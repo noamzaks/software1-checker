21fba6ead644ec67d42241ee6783d206e70cd3ea
Noam Zaks <imnoamzaks@gmail.com>
2022-11-02 18:07:47 +0200

    automatic update, fancy

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,25 @@
+use std::error::Error;
 use std::path::Path;
 use std::process::exit;
 use std::{env, process};
 
+use loading::Loading;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Test {
+    arguments: Vec<String>,
+    input: String,
+    output: String,
+}
+
+#[derive(Deserialize, Debug)]
 struct Assignment {
-    filename: &'static str,
+    filename: String,
     tests: Vec<Test>,
 }
 
-struct Test {
-    arguments: Vec<&'static str>,
-    input: &'static str,
-    output: &'static str,
-}
-
-fn main() {
-    let tests = [
-        Vec::from([Assignment {
-            filename: "",
-            tests: Vec::from([]),
-        }]),
-        Vec::from([
-            Assignment {
-                filename: "Assignment02Q01.java",
-                tests: Vec::from([
-                    Test {
-                        arguments: Vec::from(["Before", "A", "E", "none"]),
-                        input: "",
-                        output: "A\nn\n",
-                    },
-                ]),
-            },
-            Assignment {
-                filename: "Assignment02Q02.java",
-                tests: Vec::from([
-                    Test {
-                        arguments: Vec::from(["4"]),
-                        input: "",
-                        output: "2.8952380952380956 3.141592653589793\n",
-                    },
-                    Test {
-                        arguments: Vec::from(["100"]),
-                        input: "",
-                        output: "3.1315929035585537 3.141592653589793\n",
-                    },
-                ]),
-            },
-            Assignment {
-                filename: "Assignment02Q03.java",
-                tests: Vec::from([
-                    Test {
-                        arguments: Vec::from(["10"]),
-                        input: "",
-                        output: "The first 10 Fibonacci numbers are:\n1 1 2 3 5 8 13 21 34 55\nThe number of odd numbers is: 7\n",
-                    },
-                    Test {
-                        arguments: Vec::from(["20"]),
-                        input: "",
-                        output: "The first 20 Fibonacci numbers are:\n1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765\nThe number of odd numbers is: 14\n",
-                    },
-                ]),
-            },
-            Assignment {
-                filename: "Assignment02Q04.java",
-                tests: Vec::from([
-                    Test {
-                        arguments: Vec::from([]),
-                        input: "",
-                        output: "[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]\n",
-                    }
-                ]),
-            },
-            Assignment {
-                filename: "Assignment02Q05.java",
-                tests: Vec::from([
-                    Test {
-                        arguments: Vec::from(["3", "1", "2", "3", "4", "5", "6", "7", "8", "9"]),
-                        input: "",
-                        output: "[1, 2, 3]\n[4, 5, 6]\n[7, 8, 9]\n\n[7, 4, 1]\n[8, 5, 2]\n[9, 6, 3]\n",
-                    },
-                    Test {
-                        arguments: Vec::from(["4", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]),
-                        input: "",
-                        output: "[1, 2, 3, 4]\n[5, 6, 7, 8]\n[9, 10, 11, 12]\n[13, 14, 15, 16]\n\n[13, 9, 5, 1]\n[14, 10, 6, 2]\n[15, 11, 7, 3]\n[16, 12, 8, 4]\n",
-                    }
-                ]),
-            },
-        ]),
-    ];
-
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
@@ -98,6 +30,16 @@ fn main() {
         exit(-1);
     }
 
+    let mut total_passing = 0;
+    let mut total_failing = 0;
+    let loading = Loading::default();
+    loading.text("Fetching latest tests");
+
+    let tests = reqwest::blocking::get("https://gist.githubusercontent.com/noamzaks/38e9723f7f131ce2e2363957afa8324e/raw/tests.json")?.text()?;
+    let tests: Vec<Vec<Assignment>> = serde_json::from_str(&tests).unwrap();
+    loading.success("Fetched latest tests");
+    loading.end();
+
     let directory = &args[1];
     let root = Path::new(directory);
     env::set_current_dir(root).unwrap();
@@ -107,13 +49,14 @@ fn main() {
         let filename = format!("hw{}-{}", i, user);
         let path = Path::new(&filename);
         if path.is_dir() {
-            println!("Checking Homework #{}", i);
             env::set_current_dir(path.join("src")).unwrap();
             for assignment in &tests[i - 1] {
-                println!("Checking {}", assignment.filename);
+                let loading = Loading::default();
+                loading.text(format!("Checking {}", assignment.filename));
+
                 for test in &assignment.tests {
                     let output = process::Command::new("java")
-                        .arg(assignment.filename)
+                        .arg(&assignment.filename)
                         .args(&test.arguments)
                         .output();
 
@@ -121,16 +64,24 @@ fn main() {
                         Ok(output) => {
                             let actual_output = String::from_utf8(output.stdout).unwrap();
                             if actual_output == test.output {
-                                println!("Test passed");
+                                loading.success("Test passed");
+                                loading.end();
+                                total_passing += 1;
                             } else {
+                                loading.fail("Test failed");
+                                loading.end();
                                 println!(
-                                    "Test failed: Expected:\n----------------\n{}\n----------------\nGot:\n----------------\n{}\n----------------",
+                                    "Expected:\n----------------\n{}\n----------------\nGot:\n----------------\n{}\n----------------",
                                     test.output, actual_output
                                 );
+                                total_failing += 1;
                             }
                         }
                         Err(error) => {
                             println!("Test failed: {}", error);
+                            loading.fail(format!("{} Incorrect", assignment.filename));
+                            loading.end();
+                            total_failing += 1;
                         }
                     }
                 }
@@ -138,4 +89,15 @@ fn main() {
             env::set_current_dir(root).unwrap();
         }
     }
+
+    if total_failing == 0 {
+        println!("All {} tests passed!", total_passing);
+    } else {
+        println!(
+            "Results: {} failed, {} succeeded",
+            total_failing, total_passing
+        );
+    }
+
+    Ok(())
 }
